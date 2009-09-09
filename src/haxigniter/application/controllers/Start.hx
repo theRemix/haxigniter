05520fab11f7e927dffb2e490e22483837396f90
Andreas Soderlund <ciscoheat@gmail.com>
2009-09-09 23:14:26 +0000

    Added CRUD methods to DatabaseConnection class.

--- src/haxigniter/application/controllers/Start.hx
+++ src/haxigniter/application/controllers/Start.hx
@@ -5,12 +5,34 @@ import haxigniter.libraries.Debug;
 import haxigniter.libraries.Server;
 import haxigniter.types.TypeFactory;
 
+import php.db.ResultSet;
+
 class Start extends haxigniter.libraries.Controller
 {
 	public var test : Bool;
 	
 	public function index(id : Int)
 	{
+		var where = new Hash<Dynamic>();
+		var args = new Hash<Dynamic>();
+		/*
+		args.set('name', 'haXigniter');
+		args.set('sortorder', 0);
+		args.set('userid', 'HTTPS');
+		
+		var result = this.DB.Insert("items", args);
+		*/
+		
+		where.set('id', 48);
+		var result = this.DB.Delete('items', where, 1);
+		
+		Debug.trace(result);
+		Debug.trace(this.DB.LastQuery);
+		
+		//php.Lib.print(result.name + ' ' + result.letter + '<br>');
+		
+		return;
+		
 		var template : String = 'The habitants of <em>::name::</em> are : <ul>::foreach users::<li>::name:: - ::if (age > 18)::Grownup::elseif (age <= 2)::Baby::else::Young::end::</li>::end::</ul>';
 		/*
 		var input = { name : 'Turvia', users: {
--- src/haxigniter/libraries/Database.hx
+++ src/haxigniter/libraries/Database.hx
@@ -13,7 +13,16 @@ enum DatabaseDriver
 	Sqlite;
 }
 
-class DatabaseException extends Exception {}
+class DatabaseException extends Exception
+{
+	public var Connection : DatabaseConnection;
+	
+	public function new(message : String, connection : DatabaseConnection)
+	{
+		this.Connection = connection;
+		super(message);
+	}
+}
 
 // TODO: Debug query on error
 class DatabaseConnection
@@ -27,16 +36,23 @@ class DatabaseConnection
 	public var Driver : DatabaseDriver;
 	public var Debug : Bool;
 
+	public var Connection : Connection;
+
 	private var enabled : Bool;
-	private var connection : Connection;
+
+	public var LastQuery(getLastQuery, null) : String;
+	private var lastQuery : String;
+	private function getLastQuery() : String { return this.lastQuery; }
+
+	private static var AlphaRegexp : EReg = ~/^\w+$/;
 	
 	public function Open() : Void
 	{
-		if(this.connection != null)
-			throw new DatabaseException('Connection is already open.');
+		if(this.Connection != null)
+			throw new DatabaseException('Connection is already open.', this);
 		
 		if(this.Driver == DatabaseDriver.Mysql)
-			this.connection = php.db.Mysql.connect({ 
+			this.Connection = php.db.Mysql.connect({ 
 				user: this.User, 
 				socket: this.Socket, 
 				port: this.Port, 
@@ -45,24 +61,152 @@ class DatabaseConnection
 				database: this.Database
 			});
 		else // if(this.Driver == DatabaseDriver.Sqlite)
-			this.connection = php.db.Sqlite.open(this.Database);
+			this.Connection = php.db.Sqlite.open(this.Database);
 	}
 	
 	public function Close() : Void
 	{
-		if(this.connection != null)
+		if(this.Connection != null)
 		{
-			this.connection.close();
-			this.connection = null;
+			this.Connection.close();
+			this.Connection = null;
 		}
 	}
+
+	///// Query methods /////////////////////////////////////////////
 	
-	public function Query(query : String, ?params : Iterable<Dynamic> = null) : ResultSet
+	public function Query(query : String, ?params : Iterable<Dynamic>) : ResultSet
 	{
 		if(params != null)
 			query = this.queryParams(query, params);
 		
-		return this.connection.request(query);
+		this.lastQuery = query;
+		return this.Connection.request(query);
+	}
+
+	public function QueryRow(query : String, ?params : Iterable<Dynamic>) : Dynamic
+	{
+		var result = this.Query(query, params);
+		return result.hasNext() ? result.next() : {};
+	}
+
+	public function QueryInt(query : String, ?params : Iterable<Dynamic>) : Int
+	{
+		var result = this.Query(query, params);
+		return result.hasNext() ? result.getIntResult(0) : null;
+	}
+
+	public function QueryFloat(query : String, ?params : Iterable<Dynamic>) : Float
+	{
+		var result = this.Query(query, params);
+		return result.hasNext() ? result.getFloatResult(0) : null;
+	}
+
+	public function QueryString(query : String, ?params : Iterable<Dynamic>) : String
+	{
+		var result = this.Query(query, params);
+		return result.hasNext() ? result.getResult(0) : null;
+	}
+
+	///// CRUD methods //////////////////////////////////////////////
+	
+	public function Insert(table : String, data : Hash<Dynamic>, ?replace = false) : Int
+	{
+		this.testAlphaNumeric(table);
+		
+		var keys = '';
+		var values = '';
+		
+		for(key in data.keys())
+		{
+			this.testAlphaNumeric(key);
+			keys += ', ' + key;
+			values += ', ' + this.Connection.quote(Std.string(data.get(key)));
+		}
+		
+		var query = (replace ? 'REPLACE' : 'INSERT') + ' INTO ' + table + ' (' + keys.substr(2) + ') VALUES (' + values.substr(2) + ')';
+		var result : ResultSet = this.Connection.request(query);
+		
+		this.lastQuery = query;		
+		return result.length;
+	}
+
+	public function Replace(table : String, data : Hash<Dynamic>) : Int
+	{
+		return this.Insert(table, data, true);
+	}
+	
+	public function Update(table : String, data : Hash<Dynamic>, ?where : Hash<Dynamic>, ?limit : Int) : Int
+	{
+		this.testAlphaNumeric(table);
+		
+		var set = '';
+		var whereStr = '';
+		
+		for(key in data.keys())
+		{
+			this.testAlphaNumeric(key);
+			set += ', ' + key + '=' + this.Connection.quote(Std.string(data.get(key)));
+		}
+
+		if(where != null)
+		{
+			for(key in where.keys())
+			{
+				this.testAlphaNumeric(key);
+				whereStr += ' AND ' + key + '=' + this.Connection.quote(Std.string(where.get(key)));
+			}
+		}
+
+		var query = 'UPDATE ' + table + ' SET ' + set.substr(2);
+		
+		if(where != null)
+			query += ' WHERE ' + whereStr.substr(5);
+		
+		if(limit != null)
+			query += ' LIMIT ' + limit;
+
+		var result : ResultSet = this.Connection.request(query);
+		
+		this.lastQuery = query;
+		return result.length;
+	}
+	
+	public function Delete(table : String, ?where : Hash<Dynamic>, ?limit : Int) : Int
+	{
+		this.testAlphaNumeric(table);
+		
+		var whereStr = '';
+		
+		if(where != null)
+		{
+			for(key in where.keys())
+			{
+				this.testAlphaNumeric(key);
+				whereStr += ' AND ' + key + '=' + this.Connection.quote(Std.string(where.get(key)));
+			}
+		}
+
+		var query = 'DELETE FROM ' + table;
+		
+		if(where != null)
+			query += ' WHERE ' + whereStr.substr(5);
+		
+		if(limit != null)
+			query += ' LIMIT ' + limit;
+
+		var result : ResultSet = this.Connection.request(query);
+		
+		this.lastQuery = query;
+		return result.length;
+	}	
+
+	/////////////////////////////////////////////////////////////////
+	
+	private inline function testAlphaNumeric(value : String) : Void
+	{	
+		if(value == null || !DatabaseConnection.AlphaRegexp.match(value))
+			throw new DatabaseException('Invalid parameter: ' + value, this);
 	}
 	
 	private function queryParams(query : String, params : Iterable<Dynamic>)
@@ -71,9 +215,9 @@ class DatabaseConnection
 		{
 			var pos = query.indexOf('?');
 			if(pos == -1)
-				throw new DatabaseException('Not enough parameters in query.');
+				throw new DatabaseException('Not enough parameters in query.', this);
 			
-			query = query.substr(0, pos) + this.connection.quote(param) + query.substr(pos+1);
+			query = query.substr(0, pos) + this.Connection.quote(param) + query.substr(pos+1);
 		}
 		
 		return query;
