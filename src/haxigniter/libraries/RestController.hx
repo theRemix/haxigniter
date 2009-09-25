c9a88dc15a7c2fc83d60cbcf8548bca5a60f1594
Andreas Soderlund <ciscoheat@gmail.com>
2009-09-25 11:32:50 +0200

    Documented the RestController class.

--- src/haxigniter/libraries/RestController.hx
+++ src/haxigniter/libraries/RestController.hx
@@ -3,6 +3,27 @@ package haxigniter.libraries;
 import haxigniter.libraries.Controller;
 
 /**
- * Use this controller instead of Controller to get the Ruby on Rails RESTful approach for a controller. Recommended!
+ * Use this class instead of Controller to get a Ruby on Rails-inspired RESTful approach.
+ * "classname" is your controller class, "ID" is any identifier that will be casted to the type T of your choice.
+ * 
+ * GET-requests:
+ * 
+ *  /classname           -> index()
+ *  /classname/new       -> make()
+ *  /classname/ID        -> show(id : T)
+ *  /classname/ID/edit   -> edit(id : T)
+ *  /classname/ID/delete -> destroy(id : T)
+ * 
+ * POST-requests:
+ * 
+ *  /classname           -> create(formData : Hash<String>)
+ *  /classname/ID        -> update(id : T, formData : Hash<String>)
+ * 
+ * 
+ * Thanks to Thomas on the haXe list for the inspiration.
+ * 
+ * For more info about the RESTful approach, here's a tutorial:
+ * http://www.softiesonrails.com/search?q=Rest+101%3A+Part
+ * 
  */
 class RestController extends Controller {}
